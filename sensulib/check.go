362d4f4590539211d9ec0526cd95879d35efa3b7
Balazs Nagy <julsevern@gmail.com>
2019-10-23 16:02:46 +0200

    better tests, edge case

--- sensulib/check.go
+++ sensulib/check.go
@@ -11,11 +11,16 @@ type ErrorExiter interface {
 }
 
 func HandleError(err error) {
+	if err == nil {
+		Exit(0)
+	}
+
 	var exiter ErrorExiter
 	if errors.As(err, &exiter) {
 		exiter.Exit()
 		return // tests reach this
 	}
+
 	fmt.Printf("Error: %v\n", err)
 	Exit(3)
 }
